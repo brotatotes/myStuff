ec4d33ac4e898ad9326b38f0f1f1642e127a4951
Eric Hao <hao.awesome@gmail.com>
2014-03-06 13:49:44 -0700

    Added ' ' to list.

---
+++ encryptor.py
@@ -0,0 +1,10 @@
+#The Encryptor
+
+list = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'Q', 'W','E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '-', '=', '[', ']', '{', '}', '|', ';', ':', '"', ',', '.', '/', '<', '>', '?', '`', '~', ' ']
+
+
+def encrypt1(message,key):
+  encrypted_message = ''
+  for letter in message:
+    encrypted_message = encrypted_message + key[list.index(letter)]
+  return encrypted_message 
